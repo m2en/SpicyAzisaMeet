b4a0137f46738b2e7f509e898bb1601aa3239bd7
merunno <merunno92@gmail.com>
2022-02-17 10:11:32 +0900

    add: 起動まで

---
+++ src/server/index.ts
@@ -0,0 +1,20 @@
+import { Client } from 'discord.js';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+const token = process.env.DISCORD_TOKEN;
+const logChannelId = process.env.LOG_CHANNEL_ID;
+if (!token || !logChannelId) {
+  throw new Error('環境変数が指定されておらず起動に失敗しました。');
+}
+
+const client = new Client({
+  intents: ['GUILDS', 'GUILD_MEMBERS', 'GUILD_MESSAGES']
+});
+
+void client.login(token);
+
+client.on('ready', () => {
+  if (!client.user) return;
+  console.log(`${client.user.username} を起動しました。`);
+});
