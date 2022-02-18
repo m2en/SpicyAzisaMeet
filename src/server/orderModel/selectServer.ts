05e5a3f728f8704eeaccf345d142360b1a7abc60
merunno <merunno92@gmail.com>
2022-02-18 23:10:06 +0900

    add: jsonで設定できるように

--- src/server/orderModel/selectServer.ts
+++ src/server/orderModel/selectServer.ts
@@ -1,59 +1,18 @@
+/**
+ * ban-ts-comment - 理由
+ * 設定ファイル保持のため
+ */
+
 import { MessageActionRow, MessageSelectMenu } from 'discord.js';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import { selectServerData } from '../../config/SelectServer.json';
 
 export const selectServerButtonId = 'select_server';
 
-// TODO: Conflictを用意する
 export const selectServer = new MessageActionRow().setComponents([
-  new MessageSelectMenu().setCustomId(selectServerButtonId).addOptions([
-    {
-      label: 'Life',
-      description: 'アジ鯖の中心的な生活サーバー。',
-      value: 'life'
-    },
-    {
-      label: 'Vanilife',
-      description: 'Minecraftのバニラに近い生活サーバー',
-      value: 'ばにらいふ！'
-    },
-    {
-      label: 'Leon Gun War',
-      description: '全面戦争型、銃撃戦FPSサーバー。',
-      value: 'lgw'
-    },
-    {
-      label: 'Diverse',
-      description: '究極の多様性を体感せよ！！',
-      value: 'diverse'
-    },
-    {
-      label: 'A Fall New World',
-      description: 'この世界にあるのは、 岩盤一つだけ。',
-      value: 'afnw'
-    },
-    {
-      label: 'Coretol',
-      description: 'MMORPGサーバー',
-      value: 'coretol'
-    },
-    {
-      label: 'JunkGames',
-      description: 'ミニゲームサーバー',
-      value: 'jg'
-    },
-    {
-      label: 'Conflict',
-      description: '* (説明無し) *',
-      value: 'conflict'
-    },
-    {
-      label: 'CardianPVP',
-      description: '* (説明なし) *',
-      value: 'cp'
-    },
-    {
-      label: 'その他',
-      description: 'このリストにないサーバーを選択する',
-      value: '希望先未決定'
-    }
-  ])
+  new MessageSelectMenu()
+    .setCustomId(selectServerButtonId)
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+    .addOptions(selectServerData)
 ]);
